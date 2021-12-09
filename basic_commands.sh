ffe2b5b028191aba49663a771c1702f34dbea2ea
Belal Shehab <16547115+belalshehab@users.noreply.github.com>
2021-12-09 16:48:10 +0200

    Create basic_commands.sh

---
+++ basic_commands.sh
@@ -0,0 +1,16 @@
+echo ls output
+ls -a
+echo
+echo is java installed
+java --version
+echo
+echo is cmake installed
+cmake --version
+echo
+echo is git installed
+git --version
+echo
+echo is gcc installed
+gcc --version
+echo
+env
